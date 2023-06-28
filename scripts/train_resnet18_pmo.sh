2ed20cd735b6db73b6269b76b998b7bd5301ce9c
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-06-28 15:42:30 +0800

    remove bash output redirect

--- scripts/train_resnet18_pmo.sh
+++ scripts/train_resnet18_pmo.sh
@@ -6,7 +6,7 @@ export RECORDS=../datasets/tfrecords
 NAME="pmo"
 OUTNAME="pmo-moe"
 
-CUDA_VISIBLE_DEVICES=7 python train_net_pmo.py \
+CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --model.name=$NAME --model.num_clusters 8 --model.backbone resnet18_moe \
     --model.dir ../URL-experiments/saved_results/$OUTNAME \
     --model.pretrained --source ../URL-experiments/saved_results/url \
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=7 python train_net_pmo.py \
     --train.max_iter=2000 --train.summary_freq=100 \
     --train.type=standard --train.freeze_backbone --train.loss_type=task+hv \
     --train.n_mo=4 --train.hv_coefficient=1.0 --train.mo_freq=100 \
-    --train.cosine_anneal_freq=200 --train.eval_freq=200 --train.eval_size 300 \
-    1> ../URL-experiments/out/$OUTNAME.out  # 2> ../URL-experiments/out/pmo.err
+    --train.cosine_anneal_freq=200 --train.eval_freq=200 --train.eval_size 300 # \
+#    1> ../URL-experiments/out/$OUTNAME.out  # 2> ../URL-experiments/out/pmo.err
 #    2>&1
 
