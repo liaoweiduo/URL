0ddf81fb6d713401c0807e483e41c3be9dd1e12e
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-07-01 20:18:50 +0800

    try 1e-3

--- scripts/train_resnet18_pmo.sh
+++ scripts/train_resnet18_pmo.sh
@@ -4,7 +4,7 @@ export META_DATASET_ROOT=../meta-dataset
 export RECORDS=../datasets/tfrecords
 
 NAME="pmo"
-OUTNAME="pmo-moe-sgd-ce_select-cluster_nonlinear"
+OUTNAME="pmo-moe-sgd-ce_select-cluster_nonlinear-lr1e-3"
 
 CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --model.name=$NAME --model.num_clusters 8 --model.backbone resnet18_moe \
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --data.train ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
     --data.val ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
     --data.test ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
-    --train.optimizer=momentum --train.learning_rate=1e-4 --train.weight_decay=5e-5 \
+    --train.optimizer=momentum --train.learning_rate=1e-3 --train.weight_decay=5e-5 \
     --train.max_iter=1000 --train.summary_freq=50 \
     --train.type=standard --train.freeze_backbone --train.loss_type=task+pure+hv \
     --train.n_mo=10 --train.hv_coefficient=0.001 --train.mo_freq=10 \
