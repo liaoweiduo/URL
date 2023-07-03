0568e4dbd717f6b280515ab8774cd7cd9219722e
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-07-03 12:49:47 +0800

    try 1e-5

--- scripts/train_resnet18_pmo.sh
+++ scripts/train_resnet18_pmo.sh
@@ -4,7 +4,7 @@ export META_DATASET_ROOT=../meta-dataset
 export RECORDS=../datasets/tfrecords
 
 NAME="pmo"
-OUTNAME="pmo-moe-task_emb-lr1e-3-cosine"
+OUTNAME="pmo-moe-task_emb-lr1e-5-cosine"
 
 CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --model.name=$NAME --model.num_clusters 8 --model.backbone resnet18_moe \
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --data.train ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
     --data.val ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
     --data.test ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
-    --train.optimizer=adam --train.learning_rate=1e-3 --train.weight_decay=5e-5 \
+    --train.optimizer=adam --train.learning_rate=5e-5 --train.weight_decay=5e-6 \
     --train.max_iter=200 --train.summary_freq=10 \
     --train.type=standard --train.freeze_backbone --train.loss_type=task+pure+hv \
     --train.n_mo=10 --train.hv_coefficient=0.001 --train.mo_freq=10 \
