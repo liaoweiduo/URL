b8d555e64e30b45f8561a605e7d26e8194364a4b
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-07-10 13:52:17 +0800

    Update train_resnet18_pmo.sh

--- scripts/train_resnet18_pmo.sh
+++ scripts/train_resnet18_pmo.sh
@@ -4,7 +4,7 @@ export META_DATASET_ROOT=../meta-dataset
 export RECORDS=../datasets/tfrecords
 
 NAME="pmo"
-OUTNAME="pmo-onpure-mosoftmax_hard-lr5e-5"
+OUTNAME="pmo-onpure-mosoftmax_hard-lr1e-4"
 
 CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --model.name=$NAME --model.num_clusters 8 --model.backbone resnet18_moe \
@@ -13,11 +13,11 @@ CUDA_VISIBLE_DEVICES=0 python train_net_pmo.py \
     --data.train ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower \
     --data.val ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower mscoco \
     --data.test ilsvrc_2012 omniglot aircraft cu_birds dtd quickdraw fungi vgg_flower traffic_sign mscoco mnist cifar10 cifar100 \
-    --train.optimizer=adam --train.learning_rate=5e-5 --train.weight_decay=5e-6 \
-    --train.max_iter=2000 --train.summary_freq=100 \
+    --train.optimizer=adam --train.learning_rate=1e-4 --train.weight_decay=5e-6 \
+    --train.max_iter=500 --train.summary_freq=50 \
     --train.type=standard --train.freeze_backbone --train.loss_type=task+pure+hv+ce \
-    --train.n_mo=10 --train.hv_coefficient=0.001 --train.mo_freq=10 \
-    --train.cosine_anneal_freq=200 --train.eval_freq=400 --train.eval_size 50 # \
+    --train.n_mo=5 --train.hv_coefficient=0.001 --train.mo_freq=5 \
+    --train.cosine_anneal_freq=100 --train.eval_freq=100 --train.eval_size 50 # \
 #    1> ../URL-experiments/out/$OUTNAME.out  # 2> ../URL-experiments/out/pmo.err
 #    2>&1
 
