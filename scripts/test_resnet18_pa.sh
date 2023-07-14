f5c5fa0c8b54371d853d0b30ce8c965c72df032b
liaoweiduo-sustech <11849249@mail.sustech.edu.cn>
2023-07-14 13:32:08 +0800

    ip

--- scripts/test_resnet18_pa.sh
+++ scripts/test_resnet18_pa.sh
@@ -3,4 +3,4 @@ ulimit -n 50000
 export META_DATASET_ROOT=../meta-dataset
 export RECORDS=../datasets/tfrecords
 
-CUDA_VISIBLE_DEVICES=7 python test_extractor_pa.py --model.name=url --model.dir ../URL-experiments/saved_results/url
+CUDA_VISIBLE_DEVICES=7 python test_extractor_pa.py --model.name=url_repro --model.dir ../URL-experiments/saved_results/url
