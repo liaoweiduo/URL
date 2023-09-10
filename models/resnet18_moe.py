da7bc4cb971afeefc443c46965db082af70a6d30
liaoweiduo <11849249@mail.sustech.edu.cn>
2023-09-10 10:39:26 +0800

    task sim: img dist → average → task dist → softmax → task sim

--- models/resnet18_moe.py
+++ models/resnet18_moe.py
@@ -354,15 +354,18 @@ class Selector(nn.Module):
         bs = inputs.shape[0]
         embeddings = self.encoder([inputs])[0]     # [bs, 64]
         embeddings = torch.tanh(embeddings)          # apply activation on embeddings
-        if average:
-            embeddings = torch.mean(embeddings, dim=0, keepdim=True)      # [1, 64]
-            bs = 1
+        # if average:
+        #     embeddings = torch.mean(embeddings, dim=0, keepdim=True)      # [1, 64]
+        #     bs = 1
         embeddings = embeddings.view(bs, self.rep_dim, *self.prot_shape)    # [bs, 64, 1, 1]
 
         dist = self._distance(embeddings).view(bs, self.n_class)        # [bs, n_proto]  similarity
 
         if self.metric == 'euclidean':
             dist = -dist        # dist to similarity -> warning: since no sqrt, value is very large
+
+        if average:
+            dist = torch.mean(dist, dim=0, keepdim=True)      # [1, 64]
 
         # gumbel softmax or softmax
         dim = 1
