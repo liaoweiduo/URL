bff84761a4b102ae3cc4ef913a72faa1e464cd38
liaoweiduo <11849249@mail.sustech.edu.cn>
2023-09-19 23:25:10 +0800

    Update test_resnet18_pa.sh

--- scripts/test_resnet18_pa.sh
+++ scripts/test_resnet18_pa.sh
@@ -3,4 +3,6 @@ ulimit -n 50000
 export META_DATASET_ROOT=../meta-dataset
 export RECORDS=../datasets/tfrecords
 
-CUDA_VISIBLE_DEVICES=0 python test_extractor_pa.py --model.name=pmo --model.dir ../URL-experiments/saved_results/pmo-tcph--slr0_001-flr0_001
+CUDA_VISIBLE_DEVICES=0 python test_extractor_pa.py --model.name=pmo --model.dir ../URL-experiments/saved_results/pmo-tcph--slr0_001-flr0_001 \
+--model.num_clusters 10 --model.backbone resnet18_moe --model.pretrained --source ../URL-experiments/saved_results/url \
+--source_moe ../URL-experiments/saved_results/sdl
