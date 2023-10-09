b450fba163cb2ab0681deda51e4305e6fd1934e3
liaoweiduo <11849249@mail.sustech.edu.cn>
2023-10-09 18:50:19 +0800

    Update hyper_param_tune.py

--- scripts/hyper_param_tune.py
+++ scripts/hyper_param_tune.py
@@ -223,18 +223,18 @@ params = []
 """
 exp: try 1 iter = 1 tasks 
 """
-num_runs_1sh = 3        # num of runs in 1 sh file
+num_runs_1sh = 6        # num of runs in 1 sh file
 common_args.update({
-    'tag': 'pmo-tunepc',
-    'train.max_iter': 1000, 'train.summary_freq': 100, 'train.pool_freq': 10,
+    'tag': 'pmo-tune4000pc',
+    'train.max_iter': 4000, 'train.summary_freq': 500, 'train.pool_freq': 10,
     'train.mo_freq': 10, 'train.n_mo': 1,
     'train.hv_coefficient': 10,
-    'train.cosine_anneal_freq': 1000, 'train.eval_freq': 200,
+    'train.cosine_anneal_freq': 4000, 'train.eval_freq': 2000,
     'train.selector_learning_rate': 1e-4,
 })
 param_grid = {
-    'train.learning_rate': [1e-5, 1e-4, 1e-3],
-    'train.loss_type': ['task+ce+pure'],
+    'train.learning_rate': [1e-6, 1e-5, 1e-4],
+    'train.loss_type': ['task+ce+pure', 'task+ce+pure+hv'],
     # 'train.loss_type': ['task+ce+pure+hv', 'task+ce+pure', 'task+pure+hv'],
     'train.pure_coefficient': [1, 10],         # [1, 10],
 }
