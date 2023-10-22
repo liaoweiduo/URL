139a7b0dd46eec2bdc72883df13b802ee83f28cf
liaoweiduo <liaowd@mail.sustech.edu.cn>
2023-10-22 22:46:04 +0800

    debug

--- scripts/hyper_param_tune.py
+++ scripts/hyper_param_tune.py
@@ -223,30 +223,30 @@ params = []
 """
 exp: try 1 iter = 1 tasks 
 """
-num_runs_1sh = 6        # num of runs in 1 sh file
+num_runs_1sh = 4        # num of runs in 1 sh file
 common_args.update({
-    'tag': 'pmo-filmrandn-kd-balaclufilm',
+    'tag': 'pmo-filmrandn-kd-tuneslr',
     'train.max_iter': 5000, 'train.summary_freq': 500, 'train.pool_freq': 10,
     'train.mo_freq': 10, 'train.n_mo': 1, 'train.n_obj': 2, 'train.n_mix': 2,
     'train.cosine_anneal_freq': 1000, 'train.eval_freq': 1000,
-    'train.selector_learning_rate': 1e-4,
+    # 'train.selector_learning_rate': 1e-4, 
     'train.cond_mode': 'film_random',
 })
 param_grid = {
-    'train.learning_rate': [5e-4, 1e-3],
+    'train.learning_rate': [5e-4, 7e-4, 1e-3, 2e-3],
     'train.loss_type': ['task+kd+ce'],      # +pure+hv
     'train.kd_type': ['kernelcka'],
     'train.kd_T_extent': [1000],
     'train.kd_coefficient': [6],
-    'train.ce_coefficient': [2, 5, 10],
-    'cluster.logit_scale': [0.5, 1]
+    'train.ce_coefficient': [5, 10],
+    'cluster.logit_scale': [0.5]
     # 'train.pure_coefficient': [0, 0.1, 0.5, 1, 2],
     # 'train.hv_coefficient': [0, 0.1, 0.5, 1, 2],
 }
 exp_name_template = common_args['tag'] + \
                     '-lr{train.learning_rate}' + \
-                    '-cec{train.ce_coefficient}' + \
-                    '-ls{cluster.logit_scale}' # + \
+                    '-cec{train.ce_coefficient}' # + \
+                    # '-ls{cluster.logit_scale}' # + \
                     # '-kdt{train.kd_type}' + \
                     # '-kdt{train.kd_T_extent}' + \
                     # '-kdc{train.kd_coefficient}' + \
@@ -257,7 +257,7 @@ exp_name_template = common_args['tag'] + \
 params_temp = generate_params(common_args, param_grid, exp_name_template)
 for p in params_temp:
     p['train.weight_decay'] = p['train.learning_rate'] / 50
-    # p['train.selector_learning_rate'] = p['train.learning_rate']
+    p['train.selector_learning_rate'] = p['train.learning_rate']
 params.extend(params_temp)
 
 
